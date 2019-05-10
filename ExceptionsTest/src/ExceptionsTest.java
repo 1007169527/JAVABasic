d427e05fe2cd2045697d3956b926b8398601d67a
chupeng.guo <chupeng.guo@gmail.com>
2019-05-10 20:45:49 +0800

    update a project 190510

--- ExceptionsTest/src/ExceptionsTest.java
+++ ExceptionsTest/src/ExceptionsTest.java
@@ -1,36 +1,60 @@
+
+import java.util.concurrent.TimeUnit;
+
 //P562
 public class ExceptionsTest {
+	private static void delay() {
+		;
+	}
+
+	private static void delay1() {
+		try {
+			TimeUnit.MILLISECONDS.sleep(1);
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+
 	public static void main(String args[]) {
 		try {
 			throwException();
 		} catch (Exception e) {
 			// TODO: handle exception
-			System.err.println("Exception handled in main");
+			System.err.println(System.currentTimeMillis() + " 4 Exception handled in main");
+			delay();
 		}
 		doesNotThrowException();
 	}
 
 	public static void throwException() throws Exception {
 		try {
-			System.out.println("Method throwException");
+			System.out.println(System.currentTimeMillis() + " 1 Method throwException");
+			delay();
 			throw new Exception();
 		} catch (Exception e) {
-			System.err.println("Exception handled in Method throwException");
+			System.err.println(System.currentTimeMillis() + " 2 Exception handled in Method throwException");
+			delay();
 			throw new Exception();
 		} finally {
 			// TODO: handle finally clause
-			System.err.println("Finally executed in throwException");
+			System.err.println(System.currentTimeMillis() + " 3 Finally executed in throwException");
+			delay();
 		}
 	}
 
 	public static void doesNotThrowException() {
 		try {
-			System.out.println("Method doesNotThrowException");
+			System.out.println(System.currentTimeMillis() + " 5 Method doesNotThrowException");
+			delay();
 		} catch (Exception e) {
 			// TODO: handle exception
 			System.err.println(e);
+			System.out.println(System.currentTimeMillis() + " 6 Exception handled in Method doesNotThrowException");
+			delay();
 		} finally {
-			System.err.println("Finally executed in doesNotThrowException");
+			System.err.println(System.currentTimeMillis() + " 7 Finally executed in doesNotThrowException");
+			delay();
 		}
 	}
 }
