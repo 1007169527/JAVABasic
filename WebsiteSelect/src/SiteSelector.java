809aa9efb7b898b69027d53a1ed762f7b2aa3139
chupeng.guo <chupeng.guo@gmail.com>
2019-05-19 12:05:41 +0800

    add a project 190519

---
+++ WebsiteSelect/src/SiteSelector.java
@@ -0,0 +1,71 @@
+import java.applet.AppletContext;
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Vector;
+
+import javax.swing.JApplet;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+public class SiteSelector extends JApplet {
+	private HashMap sites;
+	private Vector siteNames;
+	private JList siteChooser;
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		super.init();
+		sites = new HashMap();
+		siteNames = new Vector();
+		getSitesFromHTMLParameters();
+
+		Container container = getContentPane();
+		container.add(new JLabel("Choose a site to browser"), BorderLayout.NORTH);
+
+		siteChooser = new JList(siteNames);
+		siteChooser.addListSelectionListener(new ListSelectionListener() {
+
+			@Override
+			public void valueChanged(ListSelectionEvent e) {
+				// TODO Auto-generated method stub
+				Object object = siteChooser.getSelectedValue();
+				URL newDocument = (URL) sites.get(object);
+				AppletContext browser = getAppletContext();
+				browser.showDocument(newDocument);
+			}
+		});
+
+		container.add(new JScrollPane(siteChooser), BorderLayout.CENTER);
+	}
+
+	private void getSitesFromHTMLParameters() {
+		// TODO Auto-generated method stub
+		String title, location;
+		URL url;
+		int counter = 0;
+
+		title = getParameter("title" + counter);
+
+		while (title != null) {
+			location = getParameter("location" + counter);
+			try {
+				url = new URL(location);
+				sites.put(title, url);
+				siteNames.add(title);
+			} catch (MalformedURLException e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+
+			++counter;
+			title = getParameter("title" + counter);
+		}
+	}
+}
